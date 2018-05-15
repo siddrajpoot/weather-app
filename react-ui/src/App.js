9b57cc25d59feb3b41331a81a432584e049a0cee
Sidd Rajpoot <siddrajpoot@gmail.com>
2018-05-15 10:42:58 -0700

    refactored code

--- react-ui/src/App.js
+++ react-ui/src/App.js
@@ -24,82 +24,144 @@ class App extends Component {
     };
   }
 
-  componentDidMount() {
-    let time = moment.unix(1526281200).format();
-    console.log(time);
-  }
+  componentDidMount() {}
 
   handleUserAddress(e) {
     e.preventDefault();
 
     let userAddress = document.getElementById('address').value.trim();
-    let userLat, userLng;
 
-    axios
-      .get('/api/geocode', {
-        params: {
-          userAddress
-        }
-      })
-      .then(res => {
-        let results = res.data.results;
-        console.log('Geocode:', results);
+    console.log(userAddress);
 
-        let storableLocation = {};
-        for (var ac = 0; ac < results[0].address_components.length; ac++) {
-          var component = results[0].address_components[ac];
+    this.getGeocode(userAddress).then(() => {
+      this.getDarkSky();
+    });
 
-          if (
-            component.types.includes('sublocality') ||
-            component.types.includes('locality')
-          ) {
-            storableLocation.city = component.long_name;
-          } else if (component.types.includes('administrative_area_level_1')) {
-            storableLocation.state = component.short_name;
-          } else if (component.types.includes('country')) {
-            storableLocation.country = component.long_name;
-            storableLocation.registered_country_iso_code = component.short_name;
-          }
-        }
+    // axios
+    //   .get('/api/geocode', {
+    //     params: {
+    //       userAddress
+    //     }
+    //   })
+    //   .then(res => {
+    //     let results = res.data.results;
+    //     console.log('Geocode:', results);
 
-        if (storableLocation.city && storableLocation.state) {
-          this.setState(() => ({
-            city: `${storableLocation.city}, ${storableLocation.state}`
-          }));
-        }
+    //     let storableLocation = {};
+    //     for (var ac = 0; ac < results[0].address_components.length; ac++) {
+    //       var component = results[0].address_components[ac];
 
-        userLat = results[0].geometry.location.lat;
-        userLng = results[0].geometry.location.lng;
-        axios
-          .get('/api/darksky', {
-            params: {
-              lat: userLat,
-              lng: userLng
-            }
-          })
-          .then(res => {
-            console.log('Darksky:', res.data);
-            this.setState(() => ({
-              currentTemp: Math.round(res.data.currently.temperature),
-              currentHumidity: res.data.currently.humidity * 100,
-              hourly: res.data.hourly.data,
-              daily: res.data.daily.data
-            }));
-          });
+    //       if (
+    //         component.types.includes('sublocality') ||
+    //         component.types.includes('locality')
+    //       ) {
+    //         storableLocation.city = component.long_name;
+    //       } else if (component.types.includes('administrative_area_level_1')) {
+    //         storableLocation.state = component.short_name;
+    //       } else if (component.types.includes('country')) {
+    //         storableLocation.country = component.long_name;
+    //         storableLocation.registered_country_iso_code = component.short_name;
+    //       }
+    //     }
 
-        console.log(storableLocation);
-      });
+    //     if (storableLocation.city && storableLocation.state) {
+    //       this.setState(() => ({
+    //         city: `${storableLocation.city}, ${storableLocation.state}`
+    //       }));
+    //     }
+
+    //     userLat = results[0].geometry.location.lat;
+    //     userLng = results[0].geometry.location.lng;
+    //     axios
+    //       .get('/api/darksky', {
+    //         params: {
+    //           lat: userLat,
+    //           lng: userLng
+    //         }
+    //       })
+    //       .then(res => {
+    //         console.log('Darksky:', res.data);
+    //         this.setState(() => ({
+    //           currentTemp: Math.round(res.data.currently.temperature),
+    //           currentHumidity: res.data.currently.humidity * 100,
+    //           hourly: res.data.hourly.data,
+    //           daily: res.data.daily.data
+    //         }));
+    //       });
+
+    //     console.log(storableLocation);
+    //   });
 
     document.getElementById('address-form').reset();
+  }
+
+  getGeocode(userAddress) {
+    return new Promise((resolve, reject) => {
+      axios
+        .get('/api/geocode', {
+          params: {
+            userAddress
+          }
+        })
+        .then(res => {
+          res = res.data;
+          console.log(res);
+          this.setState(() => ({
+            lat: res.results[0].geometry.location.lat,
+            lng: res.results[0].geometry.location.lng
+          }));
+
+          this.getCityFromGeocode(res.results);
+          resolve();
+        });
+    });
+  }
+
+  getCityFromGeocode(results) {
+    let storableLocation = {};
+    for (var ac = 0; ac < results[0].address_components.length; ac++) {
+      var component = results[0].address_components[ac];
+
+      if (
+        component.types.includes('sublocality') ||
+        component.types.includes('locality')
+      ) {
+        storableLocation.city = component.long_name;
+      } else if (component.types.includes('administrative_area_level_1')) {
+        storableLocation.state = component.short_name;
+      } else if (component.types.includes('country')) {
+        storableLocation.country = component.long_name;
+        storableLocation.registered_country_iso_code = component.short_name;
+      }
+    }
+
+    if (storableLocation.city && storableLocation.state) {
+      this.setState(() => ({
+        city: `${storableLocation.city}, ${storableLocation.state}`
+      }));
+    }
+  }
+
+  getDarkSky(lat, lng) {
+    axios
+      .get('/api/darksky', {
+        params: { lat: this.state.lat, lng: this.state.lng }
+      })
+      .then(res => {
+        console.log('Darksky:', res.data);
+        this.setState(() => ({
+          currentTemp: Math.round(res.data.currently.temperature),
+          currentHumidity: res.data.currently.humidity * 100,
+          hourly: res.data.hourly.data,
+          daily: res.data.daily.data
+        }));
+      });
   }
 
   render() {
     return (
       <div className="App">
         <AddressForm handleUserAddress={this.handleUserAddress} />
-        {this.state.lat && <p>Lat: {this.state.lat}</p>}
-        {this.state.lng && <p>Lng: {this.state.lng}</p>}
-        <br />
         {this.state.currentTemp && (
           <div className="main">
             <Currently
