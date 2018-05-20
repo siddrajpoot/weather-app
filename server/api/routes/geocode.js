c2ccea7de8ecff9c3ec5d6d1246bb52e8476080f
Sidd Rajpoot <siddrajpoot@gmail.com>
2018-05-19 19:22:11 -0700

    update to test branch

--- server/api/routes/geocode.js
+++ server/api/routes/geocode.js
@@ -4,15 +4,20 @@ const router = express.Router();
 const googleKey = 'AIzaSyDdm7uAJ3_SjWIRVcInUnQpzs1xILGqkDk';
 
 router.get('/', (req, res, next) => {
+  console.log('test');
   if (!req.query.userAddress) {
-    res.json({error: 'User Address was not provided'});
+    res.json({ error: 'User Address was not provided' });
   }
 
   axios
-    .get(`https://maps.googleapis.com/maps/api/geocode/json?address=${req.query.userAddress}&key=${googleKey}`)
+    .get(
+      `https://maps.googleapis.com/maps/api/geocode/json?address=${
+        req.query.userAddress
+      }&key=${googleKey}`
+    )
     .then(result => {
       res.send(result.data);
-    })
-})
+    });
+});
 
 module.exports = router;
