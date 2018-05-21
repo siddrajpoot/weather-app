c66c7e34f9350f3deac5cb7bb2f0499bc3d6a28c
Sidd Rajpoot <siddrajpoot@gmail.com>
2018-05-20 17:40:08 -0700

    adjustments to front page styling

--- react-ui/src/components/AddressForm.js
+++ react-ui/src/components/AddressForm.js
@@ -2,25 +2,15 @@ import React from 'react';
 
 const AddressForm = props => {
   return (
-    <div className="row">
-      <form
-        onSubmit={props.handleUserAddress}
-        id="address-form"
-        className="col s12"
-      >
-        <div className="row">
-          <div className="input-field col s12">
-            <input
-              className=""
-              type="text"
-              id="address"
-              placeholder="Enter a location"
-              required
-            />
-          </div>
-        </div>
-      </form>
-    </div>
+    <form onSubmit={props.handleUserAddress} id="address-form" className="">
+      <input
+        className=""
+        type="text"
+        id="address"
+        placeholder="Enter a location"
+        required
+      />
+    </form>
   );
 };
 
