bb6c22355eca84f3355c1eef39257a9fbc7d7abe
Sidd Rajpoot <siddrajpoot@gmail.com>
2018-05-21 16:16:38 -0700

    finished 10 hour

--- react-ui/src/components/Hour.js
+++ react-ui/src/components/Hour.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import moment from 'moment';
+import WeatherIcon from './WeatherIcon';
 
 const Hour = props => {
   return (
-    <div>
-      <p>
-        {moment.unix(props.time).format('h a')}, {Math.floor(props.temperature)}&deg;F
-      </p>
+    <div className="hour">
+      <div className="time-container">
+        <p className="time-hour">{moment.unix(props.time).format('h')}</p>
+        <p className="time-period">{moment.unix(props.time).format('a')}</p>
+      </div>
+      <WeatherIcon icon={props.icon} />
+      <p className="temperature">{Math.floor(props.temperature)}&deg;F</p>
     </div>
   );
 };
