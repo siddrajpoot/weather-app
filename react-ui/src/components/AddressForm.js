8749cdb16c56cf3afa82d27c38312f8f1cd84494
Sidd Rajpoot <siddrajpoot@gmail.com>
2018-05-22 10:56:21 -0700

    Added button to mobile form page

--- react-ui/src/components/AddressForm.js
+++ react-ui/src/components/AddressForm.js
@@ -1,15 +1,13 @@
 import React from 'react';
+import locationIcon from '../images/location.png';
 
 const AddressForm = props => {
   return (
     <form onSubmit={props.handleUserAddress} id="address-form" className="">
-      <input
-        className="z-depth-3"
-        type="text"
-        id="address"
-        placeholder="Enter a location"
-        required
-      />
+      <input type="text" id="address" placeholder="Enter a location" required />
+      <button className="address-submit" type="submit">
+        <img className="location-icon" src={locationIcon} />
+      </button>
     </form>
   );
 };
