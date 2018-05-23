5828b4692580c6747f36290b0effccc4404f1593
Sidd Rajpoot <siddrajpoot@gmail.com>
2018-05-22 19:39:41 -0700

    adjusted images and added submit img in correct location

--- react-ui/src/components/AddressForm.js
+++ react-ui/src/components/AddressForm.js
@@ -1,12 +1,12 @@
 import React from 'react';
-import locationIcon from '../images/location.png';
+import submitIcon from '../images/arrow.png';
 
 const AddressForm = props => {
   return (
     <form onSubmit={props.handleUserAddress} id="address-form" className="">
       <input type="text" id="address" placeholder="Enter a location" required />
       <button className="address-submit" type="submit">
-        <img className="location-icon" src={locationIcon} alt="location-icon" />
+        <img className="location-icon" src={submitIcon} alt="location-icon" />
       </button>
     </form>
   );
--- react-ui/src/components/Header.js
+++ react-ui/src/components/Header.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import logo from '../images/arrow.png';
+import logo from '../images/logo.png';
 
 export default () => (
   <div className="header">
